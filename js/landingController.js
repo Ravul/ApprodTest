a0099421144b2408f07f56bed30991bd706340c3
Ravul <raul.pop90@gmail.com>
2015-11-22 18:25:07 +0200

    First Commit

---
+++ js/landingController.js
@@ -0,0 +1,17 @@
+(function() {
+    
+var app = angular.module('app', [])
+    .controller('LandingCtrl', function() {
+        var docOptions = [
+            {first:"PFA", second:"Înființare"},
+            {first:"PFA", second:"Înființare"},
+            {first:"SRL", second:"Înființare"},
+            {first:"ANAF", second:"220"},
+            {first:"ANAF", second:"600"},
+            {first:"Alte", second:"Formulare"}
+        ]; 
+        this.docOptions = docOptions;
+    })
+;
+    
+}());
